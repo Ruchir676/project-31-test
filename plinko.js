e1c36f46a9b6f5631843d1ff8f3674d98b76a294
Ruchir676 <72313742+Ruchir676@users.noreply.github.com>
2020-12-05 21:48:13 -0500

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,20 @@
+class Plinko {
+    constructor(x,y,r) {
+      var options = {
+          isStatic: true
+      }
+      this.r = r;
+      this.body = Bodies.circle(x,y,this.r,options);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      noStroke();
+      fill(white);
+      ellipseMode(RADIUS);
+      ellipse(pos.x, pos.y, this.r, this.r);
+      pop();
+    }
+  };
